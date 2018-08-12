0230d2e8ae73358fdafc0895c4c383de07d98151
Pradyoth Rao <35117288+Pradyoth-Rao@users.noreply.github.com>
2018-08-12 12:10:34 -0400

    Add files via upload

---
+++ Angular/Ajs_Routing/js/controller.js
@@ -0,0 +1,11 @@
+module.controller("Route1Controller", function ($scope) {
+    $scope.cars = ['AUDI', 'BMW', 'MERCEDES'];
+    $scope.my = { favorite: 'AUDI' };    
+
+});
+module.controller("Route2Controller", function ($scope) {
+    $scope.clickCounter = 0;
+    $scope.check = function () {
+        $scope.clickCounter++;
+    };
+});
